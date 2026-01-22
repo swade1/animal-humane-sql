9480c1b87bcaac19320c68ebb7c3a78dbe5549e9
Susan Wade <susan.wade09@gmail.com>
2026-01-22 12:55:22 -0700

    Patched the supabase URL not found error

--- app/src/lib/supabaseClient.ts
+++ app/src/lib/supabaseClient.ts
@@ -5,5 +5,3 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-console.log("NEXT_PUBLIC_SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("NEXT_PUBLIC_SUPABASE_ANON_KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
