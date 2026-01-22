4f340311de057c2924da0bfdd06034836ebad87a
Susan Wade <susan.wade09@gmail.com>
2026-01-22 11:58:15 -0700

    Disabled Turbopack ensuring webpack is always used

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    turbo: false,
+  },
 };
 
 export default nextConfig;
