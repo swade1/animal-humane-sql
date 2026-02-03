66e2cb4f86489f24fd24b57c41b4bc20d4430423
Susan Wade <susan.wade09@gmail.com>
2026-02-02 19:48:25 -0700

    Removed test causing error

--- src/utils/run-scraper-pipeline.ts
+++ src/utils/run-scraper-pipeline.ts
@@ -1,5 +1,4 @@
 console.log('=== TOP OF run-scraper-pipeline.ts ===');
-throw new Error('Test error: pipeline script executed');
 // run-scraper-pipeline.ts
 // Orchestrates the full animal data pipeline: fetch, enrich/upsert, adoption check
 
